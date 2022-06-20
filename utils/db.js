03c02fec0dd80a1321f3b3d3cfedebefdea799d6
PetxCode <71887594+PetxCode@users.noreply.github.com>
2022-06-20 11:35:32 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const url = "mongodb://localhost/authTest";
 const url2 =
-	"mongodb+srv://classBuild:classBuild@cluster0.75kqx.mongodb.net/AuthDBTEst?retryWrites=true&w=majority";
+	"mongodb+srv://CodeLab:CodeLab@cluster0.lzdw3.mongodb.net/AuthTestDB?retryWrites=true&w=majority";
 
 mongoose.connect(url2).then(() => {
 	console.log("database is now connected...!");
