508b6956d7d66c1355bd3b14eba4807eef3bf44e
PetxCode <71887594+PetxCode@users.noreply.github.com>
2022-06-20 11:27:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+res.json({message: "This is the starting point...!"})
+})
+
 app.use("/api/user", require("./controller/userRouter"));
 
 app.listen(port, () => {
